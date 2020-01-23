3e1a4dd18a0e1a69ae5a1a2e678b0703db1bbc16
MinexAutomation <minex.automation@gmail.com>
2020-01-23 15:11:40 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Megara.Examples.Lib/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Megara.Examples.Lib
+{
+    public class Class1
+    {
+    }
+}
---
+++ source/R5T.Megara.Examples/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Megara.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
