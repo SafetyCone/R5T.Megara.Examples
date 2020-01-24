8335874e5621342606c563930987e63a15157a5a
MinexAutomation <minex.automation@gmail.com>
2020-01-23 16:09:46 -0800

    Added LinesTextSerializer ITextSerializer<List<string>> implementation.

--- source/R5T.Megara.Examples.Lib/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Megara.Examples.Lib
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Megara.Examples.Lib/Code/Services/Implementations/LinesTextSerializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using R5T.Magyar.IO;
+using R5T.Tiros;
+
+
+namespace R5T.Megara.Examples.Lib
+{
+    public class LinesTextSerializer : ITextSerializer<List<string>>
+    {
+        public List<string> Deserialize(TextReader reader)
+        {
+            var output = new List<string>();
+
+            while(reader.ReadLine(out var line))
+            {
+                output.Add(line);
+            }
+
+            return output;
+        }
+
+        public void Serialize(TextWriter writer, List<string> value)
+        {
+            foreach (var line in value)
+            {
+                writer.WriteLine(line);
+            }
+        }
+    }
+}
