bd128abf7b439de27ecd98f4468ad5c27d445fca
MinexAutomation <minex.automation@gmail.com>
2020-01-23 16:11:23 -0800

    Added Startup class. Made Program into a service. Ran the Program service to de/serialize the basic text file from Dropbox/Rivet/Testing Data to C/Temp.

--- source/R5T.Megara.Examples/Code/Program.cs
+++ source/R5T.Megara.Examples/Code/Program.cs
@@ -1,4 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Chalandri;
+using R5T.Evosmos;
+using R5T.Ilioupoli.Default;
+using R5T.Richmond;
+
 
 namespace R5T.Megara.Examples
 {
@@ -6,7 +15,45 @@ namespace R5T.Megara.Examples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serviceProvider = ServiceProviderBuilder.NewUseStartup<Startup>();
+
+            var program = serviceProvider.GetRequiredService<Program>();
+
+            program.Run();
+        }
+
+
+        private ITestingDataDirectoryContentPathsProvider TestingDataDirectoryContentPathsProvider { get; }
+        private ITemporaryDirectoryFilePathProvider TemporaryDirectoryFilePathProvider { get; }
+        private IFileSerializer<List<string>> LinesTextFileSerializer { get; }
+
+
+        public Program(
+            ITestingDataDirectoryContentPathsProvider testingDataDirectoryContentPathsProvider,
+            ITemporaryDirectoryFilePathProvider temporaryDirectoryFilePathProvider,
+            IFileSerializer<List<string>> linesTextFileSerializer)
+        {
+            this.TestingDataDirectoryContentPathsProvider = testingDataDirectoryContentPathsProvider;
+            this.TemporaryDirectoryFilePathProvider = temporaryDirectoryFilePathProvider;
+            this.LinesTextFileSerializer = linesTextFileSerializer;
+        }
+
+        public void Run()
+        {
+            var sourceBasicTextFilePath = this.TestingDataDirectoryContentPathsProvider.GetBasicTextFilePath();
+
+            var fileName = TestingDataDirectoryContentConventions.BasicTextFileNameValue;
+            var destinationFilePath = this.TemporaryDirectoryFilePathProvider.GetTemporaryDirectoryFilePath(fileName);
+
+            var lines = this.LinesTextFileSerializer.Deserialize(sourceBasicTextFilePath);
+
+            var writer = Console.Out;
+            foreach (var line in lines)
+            {
+                writer.WriteLine(line);
+            }
+
+            this.LinesTextFileSerializer.Serialize(destinationFilePath, lines);
         }
     }
 }
---
+++ source/R5T.Megara.Examples/Code/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using R5T.Chalandri.DropboxRivetTestingData;
+using R5T.Evosmos.CDriveTemp;
+using R5T.Megara.TextSerializer;
+using R5T.Richmond;
+
+using R5T.Megara.Examples.Lib;
+
+
+namespace R5T.Megara.Examples
+{
+    public class Startup : StartupBase
+    {
+        public Startup(ILogger<Startup> logger)
+            : base(logger)
+        {
+        }
+
+        protected override void ConfigureServicesBody(IServiceCollection services)
+        {
+            services
+                .AddSingleton<Program>()
+                .AddTextFileSerializer<List<string>, LinesTextSerializer>()
+                .AddTestingDataDirectoryContentPathsProvider()
+                .AddTemporaryDirectoryFilePathProvider()
+                ;
+        }
+    }
+}
