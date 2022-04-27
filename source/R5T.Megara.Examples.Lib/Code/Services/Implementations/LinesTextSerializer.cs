8489c0388b66d1459e6238fb61b9ff2f0967f794
davidcoats <david@getsafetycone.com>
2022-04-27 15:32:55 -0700

    Interim changes.

--- source/R5T.Megara.Examples.Lib/Code/Services/Implementations/LinesTextSerializer.cs
+++ source/R5T.Megara.Examples.Lib/Code/Services/Implementations/LinesTextSerializer.cs
@@ -1,14 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 
 using R5T.Magyar.IO;
-using R5T.Tiros;
+using R5T.Tiros;using R5T.T0064;
 
 
 namespace R5T.Megara.Examples.Lib
-{
-    public class LinesTextSerializer : ITextSerializer<List<string>>
+{[ServiceImplementationMarker]
+    public class LinesTextSerializer : ITextSerializer<List<string>>,IServiceImplementation
     {
         public List<string> Deserialize(TextReader reader)
         {
